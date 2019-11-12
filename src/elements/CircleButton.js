a10cd7905d5018a028a3b9ea48292e243fe53ead
uni51 <uni51@users.noreply.github.com>
2019-11-12 15:39:04 +0900

    Style MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
+
     return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonTitle}>
+      // スタイルのcircleButtonをstyleで上書きする
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
           {this.props.children}
         </Text>
       </View>
@@ -20,7 +31,6 @@ const styles = StyleSheet.create({
     right: 32,
     height: 48,
     width: 48,
-    backgroundColor: '#E31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -32,7 +42,6 @@ const styles = StyleSheet.create({
   circleButtonTitle: {
     fontSize: 32,
     lineHeight: 32,
-    color: '#fff',
   },
 });
 
