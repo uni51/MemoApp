272e86c2d50ca2ace7868334a93a79cff9a5f894
uni51 <uni51@users.noreply.github.com>
2019-11-22 16:24:18 +0900

    Create MemoEditScreen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoListScreen />
+        <MemoEditScreen />
       </View>
     );
   }
