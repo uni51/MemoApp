6d3eccc3ebc3268e3f3d205f030d66fb92293422
uni51 <uni51@users.noreply.github.com>
2019-11-23 10:26:23 +0900

    install react-navigation

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -18,7 +18,7 @@ class MemoDetailScreen extends React.Component {
           <Text>ここに詳細文が入ります</Text>
         </View>
 
-        <CircleButton name="pencil" color="white" style={styles.editButton} />
+        <CircleButton name="pencil" color="white" style={styles.editButton} onPress={() => { this.props.navigation.navigate('MemoEdit'); }} />
       </View>
     );
   }
