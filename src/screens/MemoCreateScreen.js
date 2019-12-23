d47188e4f64f488263e2cdf04b7aa2775e2ba065
uni51 <uni51@users.noreply.github.com>
2019-12-23 17:10:41 +0900

    Store memos with cloud store

---
+++ src/screens/MemoCreateScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+import firebase from 'firebase';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoCreateScreen extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    body: '',
+  }
+
+  handlePress() {
+    const { params } = this.props.navigation.state;
+    const db = firebase.firestore();
+    // db.settings({ timestampsInSnapshots: true });
+    db.collection(`users/${params.currentUser.user.uid}/memos`).add({
+      body: this.state.body,
+      createdOn: new Date(),
+    })
+      .then((docRef) => {
+        console.log(docRef.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.MemoCreateInput}
+          multiline
+          value={this.state.body}
+          onChangeText={(text) => { this.setState({ body: text }); }}
+        />
+        <CircleButton name="check" onPress={this.handlePress.bind(this)} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  MemoCreateInput: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+  },
+});
+
+export default MemoCreateScreen;
